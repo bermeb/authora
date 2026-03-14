7f6faf0dde72384604e54285f45504f108d4342a
Bernhard <bernhard@bermeb.dev>
2026-03-14 21:29:44 +0100

    Init project

---
+++ src/main/java/dev/bermeb/authora/AuthoraApplication.java
@@ -0,0 +1,13 @@
+package dev.bermeb.authora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/bermeb/authora/AuthoraApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bermeb.authora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
