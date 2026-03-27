5460502d24b8c45218ba17c8711fc1c64843ce64
Bernhard <bernhard@bermeb.dev>
2026-03-27 22:18:14 +0100

    change transactional annotation import to correct springframework

--- src/main/java/dev/bermeb/authora/security/UserDetailsServiceImpl.java
+++ src/main/java/dev/bermeb/authora/security/UserDetailsServiceImpl.java
@@ -1,13 +1,13 @@
 package dev.bermeb.authora.security;
 
 import dev.bermeb.authora.repository.UserRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.NullMarked;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
