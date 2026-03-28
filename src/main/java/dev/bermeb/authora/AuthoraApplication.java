ba67f7bc292e9282882c3b8569d7af23e1eb96f8
Bernhard <bernhard@bermeb.dev>
2026-03-28 17:07:43 +0100

    remove enable caching annotation since it is already in the caching config enabled

--- src/main/java/dev/bermeb/authora/AuthoraApplication.java
+++ src/main/java/dev/bermeb/authora/AuthoraApplication.java
@@ -9,7 +9,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableCaching
 @EnableAsync
 @EnableScheduling
 @EnableConfigurationProperties(AuthoraProperties.class)
