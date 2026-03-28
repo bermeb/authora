a496e142ba91589571dbe145fb40e72cb1cdbffd
Bernhard <bernhard@bermeb.dev>
2026-03-28 16:34:35 +0100

    add http status for auth exceptions

--- src/main/java/dev/bermeb/authora/exception/AuthException.java
+++ src/main/java/dev/bermeb/authora/exception/AuthException.java
@@ -1,8 +1,19 @@
 package dev.bermeb.authora.exception;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
 public class AuthException extends RuntimeException {
 
+    private final HttpStatus status;
+
     public AuthException(String message) {
+        this(message, HttpStatus.UNAUTHORIZED);
+    }
+
+    public AuthException(String message, HttpStatus status) {
         super(message);
+        this.status = status;
     }
 }
