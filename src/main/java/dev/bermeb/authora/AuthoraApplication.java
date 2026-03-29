6349ec57d92a10735f7cd2117f2c51a5792b2aac
Bernhard <bernhard@bermeb.dev>
2026-03-29 14:29:58 +0200

    change location of enable caching annotation

--- src/main/java/dev/bermeb/authora/AuthoraApplication.java
+++ src/main/java/dev/bermeb/authora/AuthoraApplication.java
@@ -4,12 +4,14 @@ import dev.bermeb.authora.config.AuthoraProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
+@EnableCaching
 @EnableConfigurationProperties(AuthoraProperties.class)
 public class AuthoraApplication {
 
